e5babbb03f88c95d41f4942df9d6d1a8042c44c3
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-03-11 11:57:57 +0100

    mmmmmm so difficult for no reason

--- Memory/script.js
+++ Memory/script.js
@@ -20,23 +20,52 @@ function takeIn(message, errorMsg){
     return input;
 }
 
-function createBackCard(){
+function activateCard(card){
+    console.log(card);
+}
+
+function createBackCard(id){
     let d = document.createElement("div");
-    let txt = '<img src="carta_retro.jpg" alt="retro">';
+    let txt = '<img class="carta" id="' + id + '" src="carta_retro.jpg" alt="retro" onclick="activateCard(this)">';
     d.innerHTML = txt;
     cardsb.push(d);
     return d;
 }
 
-function createCard(){
+function createCard(url, id){
+    let d = document.createElement("div");
+    let txt = '<img class="carta" id="' + id + '" src="' + url + '" alt="carta">';
+    d.innerHTML = txt;
+    d.style.visibility="hidden";
+    cards.push(d);
 }
 
 function populate(){
-    for (let index = 0; index < num; index++) {
-        let cardsCol = document.getElementById("cards" + ((index%4)+1));
-        console.log("cards" + (index%4)+1);
-        const c = createBackCard();
-        cardsCol.appendChild(c);
+    let cardsTmp = [];
+    let n1 = num * 2;
+    let n2 = num * 2;
+    while (n1 > 0) {
+        n1 -= 10;
+        fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            json.forEach(element => {
+                cardsTmp.push(element);
+            });
+            return null;
+        })
+        .then(g => {
+            for (let index = 0; index < 10; index++) {
+                if (n2 == 0) break;
+                let cardsCol = document.getElementById("cards" + ((index%4)+1));
+                if (n2 <= num) createCard(cardsTmp[index].url, n2);
+                const c = createBackCard(n2);
+                cardsCol.appendChild(c);
+                n2 -= 1;
+            }
+        })
     }
 }
 
