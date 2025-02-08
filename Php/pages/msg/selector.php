2bb73e2217026789e238950d16ffcb1d00820731
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2025-02-08 11:59:11 +0100

    www

--- Php/pages/msg/selector.php
+++ Php/pages/msg/selector.php
@@ -1,7 +1,46 @@
+<script>
+    var loggedUser = <?php echo "'$loggedUser'" ?>;
+
+    async function doFetch(body = {}) {
+        return await fetch("api/chat.php", {
+            method: "post",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }).then(res => {
+            return res.json();
+        }).then(json => {
+            return json;
+        });
+    }
+</script>
+
 <head>
     <title>Page</title>
 </head>
+
+<style>
+    body {
+        justify-items: center;
+    }
+</style>
+
 <body>
-    
+    <div id="aa">adad</div>
 </body>
+
+<script>
+    async function init() {
+        response = await doFetch({
+            "action": "selector",
+            "loggedUser": loggedUser,
+        });
+        console.log(response);
+        console.log(loggedUser);
+    }
+    init()
+</script>
+
 </html>
